c10f1b4475258aac78dff77b95e54c61161a017e
Izzatillo Abdusamatov <abdusamatovtillo8@gmail.com>
2024-07-04 12:28:30 +0500

    Add project files.

---
+++ RandomGenerator/Program.cs
@@ -0,0 +1,63 @@
+﻿using RandomGenerator.Service;
+
+namespace RandomGenerator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            GenerateRandom random = new GenerateRandom();
+
+            string isContinue;
+            do
+            {
+                Console.WriteLine("Which service do you want?");
+                Console.WriteLine("1 Random full name");
+                Console.WriteLine("2 Random Email Adress");
+                Console.WriteLine("3 Random Numbers");
+                Console.WriteLine("4 Random text");
+                Console.Write("Choose");
+
+                string userChoise = Console.ReadLine();
+                int ConvertUserChoise = Convert.ToInt32(userChoise);
+
+                Console.WriteLine("How many do you want randoms?");
+                Console.Write("Enter the number");
+
+                string userInput = Console.ReadLine();
+                int userInputValue = Convert.ToInt32(userInput);
+
+                switch (ConvertUserChoise)
+                {
+                    case 1:
+
+                        random.GenerateFullNames(userInputValue);
+                        break;
+
+                    case 2:
+
+                        random.GenerateEmailAdress(userInputValue);
+                        break;
+
+                    case 3:
+
+                        random.GenerateIntRange(userInputValue);
+                        break;
+
+                    case 4:
+                        Console.Write("Enter the number of words: ");
+                        Console.WriteLine("How many words do you want? ");
+                        string userInputText = Console.ReadLine();
+                        int userTextValue = Convert.ToInt32(userInputText);
+                        random.GenerateLoremIpsum(caunt: userInputValue, numberofWords: userTextValue);
+                        break;
+                }
+
+                Console.WriteLine("Do you want to continue? (+)");
+                isContinue = Console.ReadLine();
+
+            } while (isContinue == "+");
+            
+        }
+    }
+}
---
+++ RandomGenerator/Service/Services.cs
@@ -0,0 +1,60 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+using Tynamix.ObjectFiller;
+
+namespace RandomGenerator.Service
+{
+    internal class GenerateRandom
+
+    {
+        public void GenerateFullNames(int count)
+        {
+            RealNames realNameGenerator = new RealNames();
+            Console.WriteLine("There are some full names: ");
+
+            for (int i = 0; i < count; i++)
+            {
+                string fullNameRandom = realNameGenerator.GetValue();
+                Console.WriteLine(fullNameRandom);
+            }
+        }
+        public void GenerateEmailAdress(int caunt)
+        {
+            EmailAddresses emailAddresses = new EmailAddresses();
+            Console.WriteLine("There are some Emails: ");
+            for (int i = 0; i < caunt; i++)
+            {
+                string randomEmail = emailAddresses.GetValue();
+                Console.WriteLine(randomEmail);
+            }
+
+        }
+        public void GenerateIntRange(int caunt)
+        {
+            IntRange intRange = new IntRange();
+            Console.WriteLine("There are some random numbers: ");
+            for (int i = 0; i < caunt; i++)
+            {
+                int randomNumber = intRange.GetValue();
+                Console.WriteLine(randomNumber);
+            }
+        }
+        public void GenerateLoremIpsum(int caunt, int numberofWords)
+        {
+            Lipsum loremIpsum = new Lipsum();
+            Console.WriteLine("There are some random texts:");
+            for (int i = 0; i < caunt; i++)
+            {
+                string randomText = loremIpsum.GetValue();
+                Console.WriteLine(randomText);
+            }
+        }
+
+    }
+}
+
